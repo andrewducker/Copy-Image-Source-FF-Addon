2acbb92b588f38b8ebe4af75dedc8726e6dbfe02
Andrew Ducker <andrew@ducker.org.uk>
2011-05-02 21:24:53 +0100

    Comments added, and XPI generated.

--- data/ClickMonitor.js
+++ data/ClickMonitor.js
@@ -1,14 +1,18 @@
 this.on('click', function(node)
 {
 	var href = '<A href="'+document.URL+'">';
-
+	
+	//Replace both width and height if neither of them are set.
 	if(!node.getAttribute('width') && !node.getAttribute('height'))
 	{
 		node.setAttribute('width', node.clientWidth);
 		node.setAttribute('height', node.clientHeight);
 	}
 	
+	//Replace the src attribute with the absolute url, in case of relative paths.
 	node.setAttribute('src', node.src);
+	
+	//wrap the results with an anchor
 	var src=href+outerHTML(node)+'</A>';
 
 	
