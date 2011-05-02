5abc7ebe170224216ed4d32b4eba4473b2c0b67d
Andrew Ducker <andrew@ducker.org.uk>
2011-05-02 20:14:30 +0100

    Setting the size of the image as part of the HTML, even if it wasn't set originally.

--- data/ClickMonitor.js
+++ data/ClickMonitor.js
@@ -2,7 +2,19 @@ this.on('click', function(node)
 {
 	var href = '<A href="'+document.URL+'">';
 
+	if(!node.getAttribute('width'))
+	{
+		node.setAttribute('width', node.clientWidth);
+	}
+	
+
+	if(!node.getAttribute('height'))
+	{
+		node.setAttribute('height', node.clientHeight);
+	}
+
 	var src=href+outerHTML(node)+'</A>';
+
 	
 	postMessage(src);
 });
