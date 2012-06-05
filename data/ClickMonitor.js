f223a30b0822950d90bc33a9e2b5eba0e3745d55
Andrew Ducker <andrew@ducker.org.uk>
2012-06-05 14:39:18 +0100

    Updated to API version 1.7, starting to use outerHTML and switching away from deprecated function calls.

--- data/ClickMonitor.js
+++ data/ClickMonitor.js
@@ -1,29 +1,16 @@
-this.on('click', function(node)
+self.on('click', function(node)
 {
 	var href = '<a href="'+document.URL+'">';
-	
 	//Replace both width and height if neither of them are set.
 	if(!node.getAttribute('width') && !node.getAttribute('height'))
 	{
 		node.setAttribute('width', node.clientWidth);
 		node.setAttribute('height', node.clientHeight);
 	}
-	
 	//Replace the src attribute with the absolute url, in case of relative paths.
 	node.setAttribute('src', node.src);
-	
 	//wrap the results with an anchor
-	var src=href+outerHTML(node)+'</a>';
-
+	var src=href+node.outerHTML+'</a>';
 	
-	postMessage(src);
+	self.postMessage(src);
 });
-
-function outerHTML(node)
-{
-    var div = document.createElement('div'), h;
-    div.appendChild( node.cloneNode(true) );
-    h = div.innerHTML;
-    div = null;
-    return h;
-}
